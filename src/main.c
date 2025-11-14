7dbef1fb0fc41d32595a42f9d83056dd68f4a369
Samara020 <smn@cesar.school>
2025-11-14 06:19:23 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "batalha.h"
+#include "historia.h"
 
 int main() {
     Jogador j = {"Guilherme", 100, 20, 10, 0};
     Inimigo i = {"Corrompido", 120, 15};
 
+    iniciar_historia();
     iniciarBatalha(&j, &i);
     return 0;
 }
