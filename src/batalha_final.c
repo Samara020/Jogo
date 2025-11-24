026655536c71f42a65c0276742ce16036c473a55
Carlos Henrique <carlos.henriquegds79@gmail.com>
2025-11-24 16:59:14 -0300

    artes ascii, implemen.da historia e batalha final

--- src/batalha_final.c
+++ src/batalha_final.c
@@ -4,33 +4,172 @@
 #include "batalha.h"
 #include "timer.h"
 #include "screen.h"
+#include "historia.h"
+
 
 void contextualizacaoFinal() {
     screenClear();
     textoAnimado("\nGuilherme chega ao encontro da criatura.\n\n", 15000);
-    textoAnimado("Criatura: Pobre criança, portadora de uma pequena luz lógica, você nunca vencerá ao caos.\n", 15000);
-    textoAnimado("Relembrar a antiga língua lógica não os salvará! A luz já acabou e Diego nunca mais voltará!\n\n", 15000);
-    textoAnimado("[Guilherme]: Eu renasci daquele que jamais apagou! Você é apenas uma criatura corrompida que roubou sua luz!\n", 15000);
-    textoAnimado("Diego, eu sei que você pode me ouvir... não desista!\n\n", 15000);
-    textoAnimado("Criatura: Cale-se! Ele não pode te ouvir! ~p não pode sair ~p~q... AHHHHH cale-se!!!\n", 15000);
-    textoAnimado("Eu acabarei com isso agora mesmo!\n\n", 15000);
-    textoAnimado("[Guilherme]: Não se preocupe Diego, a luz da lógica voltará ao mundo.\n", 15000);
-    textoAnimado("Eu só preciso derrotar esse monstro e tornar minha alma equivalente à sua novamente...\n", 15000);
-    textoAnimado("Eu não posso falhar. Eu só preciso encontrar a equivalência mais uma vez...\n", 15000);
-    textoAnimado("Essa será... MINHA ÚLTIMA EQUIVALÊNCIA!\n\n", 15000);
+
+     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣄⣤⣼⣿⣿⣿⣿⣿⡟⣿⣯⣟⢳⡡⣽⡷⢻⠓⡤⢵⠭⣏⣹⡟⢷⣤⣴⣮⡿⠋⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣟⢿⡯⣻⢿⡞⢛⢝⢬⣥⣭⡇⢠⣹⣶⡥⡾⣿⣿⣽⣿⣿⣶⣦⠀⣀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⠟⡏⠻⡟⠄⢁⣼⢵⢽⣻⢗⣿⣻⣮⣿⣿⣿⣿⣿⣿⣭⢿⣯⣽⣿⡿⠷⡄⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣿⣿⣿⡿⣿⣿⣱⣻⠓⢇⢰⢒⡟⡡⢾⣿⣦⡾⣷⣷⢻⢢⣹⡿⠛⣯⡐⣌⣇⣭⣿⡿⣧⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⣰⡇⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣔⣱⣼⣼⣷⣾⣅⡴⣗⣼⣾⡕⣿⡺⠞⣿⣀⠀⠛⡏⠑⣿⣯⢍⢿⡟⣯⠳⠀\n");
+    printf("⠀⠀⠀⠀⣾⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣽⣿⣿⣿⣿⣿⣉⡙⣣⣟⡿⢟⣙⣟⠟⣿⢁⣾⣻⡿⣦⡚⢱⠴⠷⡟⠛⣻⠿⢿⢷⣀\n");
+    printf("⠀⠀⠀⠀⣿⣿⣿⡿⣿⣿⣿⡟⣿⡻⢟⣿⣶⣟⣿⣿⣿⣿⣿⣽⣿⣿⣷⣿⣣⣽⣟⡱⢾⡶⣯⢥⠀⣬⣶⡾⡷⢦⠴⢓⡾⡚⡮⢿⠦⢿\n");
+    printf("⠀⠀⢸⣤⣿⣿⣿⡍⢻⣿⣿⣿⣿⠋⠉⠀⢹⡿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣷⣿⠿⣛⠉⠿⣪⡪⢶⣿⣿⣿⣿⣿⣦⣀⠀⢹⠿⡋⢫⠹⠋\n");
+    printf("⠀⠀⠸⣿⣿⣿⠟⣇⣽⣿⢾⣹⠏⠀⠀⠀⠀⠻⢾⣦⡈⢻⣿⣿⣿⣷⣏⣾⢋⣬⠋⣆⣤⣍⣾⣿⢻⣿⣿⣿⡞⠘⠿⣦⣼⣌⣺⠗⣷⠄\n");
+    printf("⠀⢰⣾⣿⣿⡏⢫⣷⣿⠖⠚⠁⠀⠀⠀⠀⠀⠀⠀⣿⣯⣲⣿⣿⣿⣿⣟⣷⣴⠴⣼⣯⡿⢟⣿⢗⡯⣾⣷⠟⠀⠀⠀⠈⠉⠛⠾⣿⠏⠀\n");
+    printf("⠀⠈⣿⣿⢩⡙⠿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⠵⢻⣿⣿⣿⣿⣿⣿⣿⣿⣾⣽⣿⣋⣼⣿⣿⡟⢁⠄⠀⠀⠀⠀⠀⠀⢸⣏⣀⠴\n");
+    printf("⣼⣿⡷⡂⠰⣓⠸⢳⣶⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⢿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⢹⣀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡝⣸⢖\n");
+    printf("⡞⢹⣷⡮⢹⣾⡿⠻⣾⡺⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡷⣢\n");
+    printf("⡋⢸⣿⣤⢻⠁⠀⠀⠉⣷⡇⠀⠀⠀⠀⠀⠀⢀⣴⣶⣾⣿⣿⣿⣻⣹⠆⡰⠞⣽⣿⣿⣿⣿⣿⣿⣿⣶⡄⠀⠀⠀⣀⣀⢀⡠⠞⢗⠸\n");
+    printf("⡇⢸⣿⣯⣿⡄⠀⠀⠀⠋⠀⠀⠀⢀⣀⣴⣖⣛⡟⢻⣿⣿⢿⣝⣿⡥⣯⢷⡿⢿⣿⣿⣿⣿⣿⣻⡿⢿⣿⣷⠀⠀⣸⢫⢺⣝⢣⣘⣶⣷\n");
+    printf("⣿⣾⡽⠻⣿⣧⣄⣀⠀⠀⠀⢀⡠⣟⣋⡱⢀⣈⡀⠀⠙⠯⡇⡻⣿⣩⣿⣻⣿⣿⡿⣼⣿⣾⣯⣠⡽⠻⣽⣷⡄⢸⣧⡿⠛⢿⣿⢾⢟⣾\n");
+    printf("⠉⠻⣟⣄⠀⠉⠉⠁⠀⠀⡠⡟⢩⡃⢀⠗⢢⢇⠀⠀⠀⠀⠁⠇⠹⠝⠫⠋⣽⡇⣿⢿⢻⣇⣄⠞⠚⢻⣍⢻⣟⢸⡇⠀⠀⡰⣫⣥⣿⣧\n");
+    printf("⠀⠀⠀⠉⠁⠀⠀⠀⠀⡾⠛⠈⠡⠉⠸⠢⠌⠀⠀⣠⡶⣦⣄⠀⠀⠀⠀⢠⠏⢱⣿⢾⡁⢱⠃⠁⣺⣏⣉⡿⣿⡄⠁⠀⢀⣿⣶⡿⠛⡇\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⠀⢀⣴⣶⣾⣿⣿⣻⣷⠀⠀⠀⠀⠀⢸⢟⢸⣷⠋⢙⠟⠧⡥⢝⠞⠕⡇⠀⢀⣾⡿⠋⢰⣮⣶\n");
+    printf("⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀⠀⢀⡠⠄⣻⣿⣧⠞⢈⣽⠀⠀⣤⠈⠀⣇⢻⣾⣏⣂⡼⣀⡜⣹⢋⢎⢾⣷⣀⠉⠁⠀⢀⣼⣵⠿\n");
+    printf("⠀⠀⠀⠀⠀⠀⠈⡄⠀⠀⠀⠀⠀⠀⣀⣵⣶⣼⣿⣿⣶⣷⣿⣿⣄⣰⠙⣄⢠⠙⣍⣳⣿⠹⠁⠀⡽⠡⢣⠞⢦⡏⢿⣄⣀⠚⠛⠋⠡⠾\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠐⢄⣀⣴⣶⣶⣿⣿⣿⣿⣿⣽⣿⡿⣿⡕⢻⠙⣟⣧⡸⣸⠀⠈⠦⣯⣧⢧⠜⡐⣡⡗⢆⡸⡗⠊⢼⠚⡦⣤⢤⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⢻⠿⠟⡫⢫⢏⣿⣧⡶⢷⢿⣿⣇⠈⠀⠀⠀⢸⣼⠏⠜⡴⠁⢨⣂⡷⣇⢴⠁⠀⠰⠀⠋⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⣞⣿⣿⣿⣿⣿⣿⡿⢸⡴⠋⡴⠁⡼⣽⣿⣾⣿⣿⣿⠇⠀⠀⠀⠀⣠⢃⢎⣼⣻⣿⣯⣿⣇⡼⢦⣈⡽⠓⢀⠄⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⢸⣽⣿⣿⣿⣿⣿⠛⠇⢀⣴⠞⠁⣰⠃⢨⣿⣿⣿⠿⢿⣦⡀⠀⠀⡴⣣⢏⢾⣷⣽⠌⢱⢻⣿⡹⣴⢿⣄⠉⠉⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠸⣼⣿⣿⣿⣿⣿⣠⣴⡿⠁⠀⣰⣿⣶⣿⣫⢿⣣⣤⣜⣿⣳⣕⣾⣴⢯⠏⢰⣿⢿⠑⣠⡋⣗⢳⢿⣿⢹⠀⠀⠀⠀⠀\n");
+    printf("⣤⣴⣶⣶⣶⣦⣤⣷⣿⣿⣿⣿⡿⠟⢁⡴⠁⣰⣿⣿⣿⣿⣧⠈⠛⠿⣟⣯⣌⡿⠛⢿⣵⣤⠈⣏⠢⣳⡈⠩⡜⠝⣺⢿⢾⡄⠀⠀⠀⠀\n");
+    printf("⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⢀⡜⠱⣱⣿⣿⣿⣿⢿⣿⡀⠀⠀⠀⠙⣿⣧⡠⠋⠈⠁⠀⠘⢧⡨⡢⣈⠀⠘⣠⣾⣋⠇⠀⠀⠀⠀\n");
+    printf("⣿⣿⣿⡿⠛⠋⠁⠀⠀⠀⡰⠂⣠⣯⢌⣽⣿⣿⣿⣿⣿⣿⣎⠷⣄⠀⠀⣞⣽⠃⠀⠀⠀⠀⠀⠀⠀⠉⠫⡒⠉⠄⠋⡻⣿⠀⢀⠠⠀⠀\n");
+    printf("⣿⣿⣿⣿⣷⣶⣄⠀⠀⣠⣮⠀⠾⣽⣿⣿⣿⣿⣿⣿⣿⣿⡷⢈⣦⣬⣷⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀⠂⠀⠈⠐⡅⠀⠀⠀⠀\n");
+    printf("⣿⣿⠟⠋⢄⣰⣷⣾⣿⣿⡤⣴⣿⣿⣿⣿⣿⢿⣿⣿⣿⣷⣿⠋⠀⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢀⡈⡀⠀⢷⡀⠀⠀⠀\n");
+    printf("⠈⠀⡀⠀⠀⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠛⠃⠀⠙⢿⣅⠀⠀⠀⢘⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡔⠡⡪⠢⢄⢽⡃⠀⠀⠀\n");
+    printf("⠀⠀⢗⡀⢠⠀⠀⠉⠉⠉⣶⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢙⣷⣿⣽⣯⠷⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡰⢡⡶⢀⠲⢱⢧⠀⠀⠀\n");
+    printf("⠻⢷⣌⣥⣏⣞⢤⢄⣠⣿⣿⣿⣿⣿⠟⠀⠀⠀⠓⠀⠀⠀⢨⠯⠑⡤⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⢀⢴⣒⢯⢩⠝⠟⣒⡪⣯⡬⣗⣦⡄\n");
+
+
+         printf("\033[5;81H");
+    textoAnimado("Criatura: Pobre criança, portadora de uma pequena luz lógica,", 50000);
+    
+    printf("\033[7;81H");
+    textoAnimado("Relembrar a antiga língua lógica não os salvará!", 50000);
+    printf("\033[8;81H"); // Continuação na linha de baixo, mesma coluna
+    textoAnimado("A luz já acabou e Diego nunca mais voltará!", 50000);
+    
+    printf("\033[10;81H");
+    textoAnimado("[Guilherme]: Eu renasci daquele que jamais apagou!", 50000);
+    printf("\033[11;81H");
+    textoAnimado("Você é apenas uma criatura corrompida que roubou sua luz!", 50000);
+    
+    printf("\033[13;81H");
+    textoAnimado("Diego, eu sei que você pode me ouvir... não desista!", 50000);
+    
+    printf("\033[15;81H");
+    textoAnimado("Criatura: Cale-se! Ele não pode te ouvir!", 50000);
+    printf("\033[16;81H");
+    textoAnimado("~p não pode sair ~p~q... AHHHHH cale-se!!!", 50000);
+    
+    printf("\033[18;81H");
+    textoAnimado("Eu acabarei com isso agora mesmo!", 50000);
+    
+    printf("\033[20;81H");
+    textoAnimado("[Guilherme]: Não se preocupe Diego, a luz da lógica voltará ao mundo.", 50000);
+    
+    printf("\033[22;81H");
+    textoAnimado("Eu só preciso derrotar esse monstro e tornar", 50000);
+    printf("\033[23;81H");
+    textoAnimado("minha alma equivalente à sua novamente...", 50000);
+    
+    printf("\033[25;81H");
+    textoAnimado("Eu não posso falhar. Eu só preciso encontrar a equivalência mais uma vez...", 50000);
+
+      printf("\033[27;81H");
+    textoAnimado("Essa será... MINHA ÚLTIMA EQUIVALÊNCIA!", 100000);
+
+    
+    printf("\033[36;1H");
+    
+    
 }
 
 int perguntaFinalUnica(int n) {
     char resposta[256];
     screenClear();
     textoAnimado("DESAFIO FINAL - A ÚLTIMA EQUIVALÊNCIA\n\n", 15000);
+    
 
-    if (n == 1) textoAnimado("1) Mostre a equivalência entre ~(p v q) e ~p ^ ~q\n\n", 15000);
-    if (n == 2) textoAnimado("2) Reescreva p -> q usando apenas v e ~\n\n", 15000);
-    if (n == 3) textoAnimado("3) Mostre que p <-> q é equivalente a (p -> q) ^ (q -> p)\n\n", 15000);
-    if (n == 4) textoAnimado("4) Use De Morgan para transformar ~(p ^ q)\n\n", 15000);
-    if (n == 5) textoAnimado("5) Mostre uma equivalência válida para ~(p -> q)\n\n", 15000);
+      printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⢀⡀⠀⠀⠀⠀⠀⢀⣾⠁⠀⠀⣼⣠⣾⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⣴⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡇⣠⣶⣟⣤⣄⠀⣀⣄⣤⣾⣿⣾⣿⡿⣻⣿⡾⣏⣗⣿⣯⣟⣻⣾⡟⣻⣿⣿⣿⣿⣿⣿⣿⣤⢀⣤⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣆⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡔⣿⣿⣾⡟⣿⠟⣻⢗⣯⣷⣿⠿⢾⣏⢙⣛⡲⢛⣿⣿⣿⣿⣿⣿⣧⡄⣀⣤⣤⣤⣤⡀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣾⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣽⣿⣿⡗⠊⢐⣎⣟⡮⣳⣤⣷⣿⢛⡏⢻⢱⣹⣧⢦⣾⣟⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⢾⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣿⢯⡀⡢⡎⣏⢞⣼⣿⣿⣿⣿⡿⣧⣿⢺⣻⡽⠭⢶⣋⣿⡿⣿⣿⣿⣿⣄⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣯⣿⣯⣿⡿⣿⢿⣷⣻⣿⣿⣿⣿⣯⡇⡓⡟⡣⡼⡟⢃⣤⣿⣿⡿⣿⣿⣿⣹⣼⣳⣾⡺⣽⣡⠯⢭⣶⢻⡽⣿⣿⣿⣿⣶⣶⠆\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣿⣿⣿⣿⣿⣧⣟⡧⠿⢇⠿⠟⠀⠀⠣⠿⠿⣿⣿⣿⡿⣄⣛⣻⢿⢠⠟⣻⣿⣛⣿⣿⠿⣿⣼⣟⣿⣿⣿⢟⣿⣜⣠⠃⢃⣿⣿⣿⣿⣿⣿⠄⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⢿⣿⣿⣻⣽⡷⡿⡗⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⣿⣷⣾⣿⢝⡿⡿⣿⣿⣿⡟⣟⢱⣾⣿⣿⣿⣿⣿⣿⣾⣥⣵⡓⠍⢻⢿⣣⣷⣿⣿⣿⣦⠄\n");
+    printf("⠀⠀⠀⠀⠀⠀⣿⣿⢻⣟⣿⡿⠛⠛⠋⠻⡽⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣷⣯⣷⣿⡿⣿⡯⢲⣌⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣭⡹⠃⢰⡞⡳⣯⣿⣿⣻⣄\n");
+    printf("⠀⠀⠀⠀⠀⠀⢹⡟⠈⢿⣿⠂⠀⠀⠀⢘⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣿⣿⢟⢁⣴⡟⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠷⠿⢭⡓⡲⠯⢬⣟⠫⡽⣿⣟⣾\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠓⠀⠀⠉⠓⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢙⢕⡴⡿⣻⣼⣿⣿⣿⣿⣯⣿⢿⣿⣿⣿⣿⡇⠸⠡⡐⠡⢀⠁⣦⢾⡙⢭⣿⣿⢿\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣯⢼⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⢔⠑⠻⣿⣳⡽⣿⣿⢿⣻⣽⣻⢿⡿⣿⣿⣿⣿⣥⡁⠀⡴⡡⠗⢀⢄⣧⣝⣯⡫⣝⡿\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⠻⠄⡀⠀⠀⠀⠠⣀⣄⠴⠁⠀⠀⠀⠀⠀⠙⢳⣿⣯⣯⡷⣯⣟⣿⣿⣿⣿⣿⣿⣷⡸⠉⢀⣌⠞⢎⣾⣿⣟⣿⣟⣡\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⡿⣿⣷⣳⣮⣭⣿⣿⣗⣶⣽⠾⠥⠀⠀⠀⠀⠀⠀⣠⣴⣷⣿⣷⣻⢷⣞⣯⣿⣟⣯⣿⣿⠋⡠⡴⢪⡈⠶⡛⣫⣿⣿⣿⣿⣿\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⢀⣴⣿⣻⣟⣷⣿⣿⣿⣿⣿⣭⣽⣿⣿⡝⠉⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣿⣿⣿⡾⣷⣻⢾⣟⣯⣏⡳⣤⠙⢙⣄⣖⣵⡿⣷⣿⣿⣿⣿\n");
+    printf("⠀⠀⠀⢀⣤⣶⡿⣿⣟⣿⣻⣿⣿⣿⡿⣿⣿⣿⢿⣿⣟⣾⣿⠟⠟⠛⠙⠊⠀⠀⠀⠀⠀⠀⢀⣼⣿⡿⣽⣷⣻⡽⣟⣿⣻⣿⣗⣯⢽⡺⢽⡺⢯⡄⢟⡉⡹⣤⣟⣿⣿⣿⣿⣿⣻\n");
+    printf("⠀⠀⠀⠟⠛⠙⣟⣷⣻⣾⢿⣻⣽⣷⣿⣿⡿⠁⣸⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡿⣯⡟⣵⣳⢏⡿⣹⢞⡳⠏⠊⠁⠉⠃⣿⣝⣓⣴⠊⣙⡾⣿⣿⣿⣿⣿⣿⡿⢏\n");
+    printf("⠀⠐⠄⠀⠀⠀⠻⣯⣿⠟⠛⣽⣾⣿⣿⣿⣠⣾⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠰⣿⣻⡵⡻⣵⡫⢟⡶⡙⡏⣱⠀⠀⠀⢀⣾⢽⡮⢗⣟⢍⡽⢷⡿⣿⣿⣿⣯⠄⠃⢉\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⣿⣿⠛⢠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⣴⠀⠀⠀⠀⠀⠀⠙⣷⢻⣽⢲⢹⠙⠒⢡⠈⠉⢢⠀⣴⣯⣷⣾⣽⡏⣭⣽⡖⣯⣽⣿⡟⠉⠀⢠⢠⠉\n");
+    printf("⠀⠀⠰⡁⠂⠁⠀⠀⠒⠀⠀⠀⠀⠀⠤⠋⣸⣿⣿⠏⠉⠃⠀⠉⠙⣿⣿⢴⠇⠀⠀⠀⠀⠀⠀⠀⠑⠛⠪⣭⠛⣰⣤⡤⣴⣻⠋⡝⢳⣴⣺⣫⠝⣾⡋⣭⡩⣹⣟⠀⠀⠐⢀⡠⠁\n");
+    printf("⠀⢢⡀⠀⠀⠀⡀⠡⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⠁⠀⠀⠀⠀⠀⠀⣿⠼⡏⠁⠀⠀⠀⠀⠀⢀⣀⣠⣤⠾⣠⣿⣿⣆⣷⣿⣶⣦⣝⣿⣿⣿⣭⢥⣝⣿⣗⡭⢴⣻⡀⣠⣶⠟⣀⢀\n");
+    printf("⢤⠙⣿⡄⢀⠔⡀⠀⣆⠂⠀⠖⢀⣿⣿⢟⠋⠁⢀⠄⣀⠀⠀⠀⠀⢻⡹⣏⢂⡂⣀⡸⢄⣤⣟⡼⡛⣤⣎⣿⡿⣿⣿⣿⢯⣾⣿⣹⣿⡿⢟⡛⣾⣧⣿⡯⠜⢮⣷⣿⣿⡿⡟⠍⠉\n");
+    printf("⠀⠀⢿⣿⣆⠀⣠⡁⠪⢔⡀⣠⣿⠟⠁⠀⠀⠈⠀⡀⠋⠩⠈⠒⠛⢻⣛⣯⠈⠘⠒⠋⠛⠋⠉⠁⠀⠀⠀⠛⠱⠛⠉⠹⠸⠛⠛⢙⠷⠭⠅⠛⢋⣩⠅⠀⠈⣼⣿⣿⠏⢈⣥⣬⣶\n");
+    printf("⣿⣷⣬⣿⣿⠿⣿⣭⣵⣮⢭⣿⣿⣦⣾⣶⣶⣿⣿⣿⡷⣼⣶⠶⠢⠾⠿⠿⠟⠓⠖⠊⠂⠳⠅⠂⠠⠀⢀⠀⠀⢀⠀⠀⠀⠀⢀⣞⢰⣀⣠⣴⡟⡁⡐⢂⣾⣿⣿⣿⣶⣿⣿⣿⣿\n");
+    printf("⣤⣽⡟⠻⣿⣿⣾⣿⣿⠛⠩⣤⢿⣿⣏⢯⡹⠗⠩⠙⠃⡁⠰⠀⠀⡀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠂⠑⠂⠣⣀⠀⢀⢢⡐⠍⣿⣏⡴⢡⣿⣿⣩⠟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
 
+    if (n == 1) {
+        printf("\033[10;81H");
+        textoAnimado("1) A criatura distorce a lógica e tenta te confundir", 15000);
+        printf("\033[11;81H");
+        textoAnimado("dizendo que ~(p v q) não é equivalente a ~p ^ ~q", 15000);
+        printf("\033[13;81H");
+        textoAnimado("Mostre que são equivalentes.", 15000);
+    }
+
+    if (n == 2) {
+        printf("\033[10;81H");
+        textoAnimado("2) A voz de Diego ecoa em sua mente,", 15000);
+        printf("\033[11;81H");
+        textoAnimado("lembrando o princípio da implicação.", 15000);
+        printf("\033[13;81H");
+        textoAnimado("Reescreva p -> q usando apenas os conectivos v e ~.", 15000);
+    }
+
+    if (n == 3) {
+        printf("\033[10;81H");
+        textoAnimado("3) Para recuperar parte da essência de Diego,", 15000);
+        printf("\033[11;81H");
+        textoAnimado("você deve provar que p <-> q", 15000);
+        printf("\033[13;81H");
+        textoAnimado("é equivalente à combinação (p -> q) ^ (q -> p).", 15000);
+    }
+
+    if (n == 4) {
+        printf("\033[10;81H");
+        textoAnimado("4) O caos tenta inverter sua percepção do mundo.", 15000);
+        printf("\033[12;81H");
+        textoAnimado("Use a antiga lei de De Morgan para transformar ~(p ^ q)", 15000);
+        printf("\033[13;81H");
+        textoAnimado("e restaurar a clareza.", 15000);
+    }
+
+    if (n == 5) {
+        printf("\033[10;81H");
+        textoAnimado("5) Um grito ecoa: 'Você não compreende", 15000);
+        printf("\033[11;81H");
+        textoAnimado("o verdadeiro significado de ~(p -> q)!'", 15000);
+        printf("\033[13;81H");
+        textoAnimado("Mostre uma equivalência válida para ~(p -> q).", 15000);
+    }
+
+    // Posiciona o input também ao lado (Linha 16, Coluna 81)
+    printf("\033[16;81H");
     printf("Guilherme: ");
     fflush(stdout);
     if (!fgets(resposta, sizeof(resposta), stdin)) return 0;
@@ -49,7 +188,16 @@ int perguntaFinalUnica(int n) {
 void iniciarBatalhaFinal(Jogador *jogador) {
     if (jogador->vida <= 0) return;
 
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+
     jogador->vida = 100;
+
+    
+    Inimigo chefeFinal;
+    strcpy(chefeFinal.nome, "Criatura do Caos");
+    chefeFinal.vida = 100;
+    chefeFinal.ataque = 20;
 
     contextualizacaoFinal();
 
@@ -58,18 +206,32 @@ void iniciarBatalhaFinal(Jogador *jogador) {
     while (!timerTimeOver());
 
     for (int i = 1; i <= 5; i++) {
+        printf(C_CYAN C_BOLD "\nDesafio Final %d/5:\n" C_RESET, i);
+        exibirStatus(jogador, &chefeFinal);
+        
         int correta = perguntaFinalUnica(i);
-        if (correta) {
-            textoAnimado("\nVocê acertou. A luz aumenta...\n", 15000);
+                if (correta) {
+            printf("\033[18;81H");
+            textoAnimado(C_GREEN "Você acertou. A luz aumenta..." C_RESET, 50000);
+            chefeFinal.vida -= 20;
         } else {
+            printf("\033[18;81H");
             jogador->vida -= 20;
-            textoAnimado("\nVocê errou... sua luz enfraquece...\n", 15000);
+            textoAnimado(C_RED "Você errou... sua luz enfraquece..." C_RESET, 50000);
         }
+        
+        // Pausa para o jogador ler o feedback antes de limpar a tela
+        timerInit(2000);
+        while (!timerTimeOver());
+        
         if (jogador->vida <= 0) {
-            textoAnimado("\nVocê caiu... o caos domina tudo...\n", 15000);
+            screenClear();
+            textoAnimado("\nVocê caiu... o caos domina tudo...\n", 50000);
+            mostrarGameOver();
             return;
         }
     }
-
-    textoAnimado("\nVocê libertou Diego! A lógica renasce!\n", 15000);
+    printf("\033[20;81H");
+    textoAnimado("\n" C_GREEN C_BOLD "Você libertou Diego! A lógica renasce!\n" C_RESET, 100000);
+    historiaFinal();
 }
--- src/historia.c
+++ src/historia.c
@@ -49,3 +49,50 @@ void iniciar_historia() {
     timerInit(2000);
     while (!timerTimeOver());
 }
+void historiaFinal() {
+    screenClear();
+    textoAnimado("Criatura: Você... você não irá fazer com que a lógica retorne... O caos já está instaurado, não há mais esperança.\n\n", 40000);
+
+    textoAnimado("Guilherme: Até mesmo criaturas como você vieram da luz lógica. Eu espero que encontre a paz... e talvez entenda, de forma lógica, o porquê de tudo isso. Vá em paz.\n\n", 40000);
+
+    textoAnimado("O silêncio cai sobre o campo devastado. O caos se desfaz como fumaça fria, consumido pela Última Equivalência restaurada.\n", 40000);
+    textoAnimado("A luz lógica pulsa na mão de Guilherme como uma brasa viva.\n\n", 40000);
+
+    textoAnimado("Os restos da criatura estremecem. O corpo, feito de caos, começa a rachar como carvão quente.\n", 40000);
+    textoAnimado("Uma lágrima escorre por seu rosto antes de se desfazer em partículas cinzentas que sobem ao céu.\n\n", 40000);
+
+    textoAnimado("No centro do clarão silencioso... uma figura surge.\n", 40000);
+    textoAnimado("Diego aparece.\n\n", 40000);
+
+    textoAnimado("Seu corpo translúcido brilha como vidro quente, irradiando uma luz suave e firme.\n", 40000);
+
+    textoAnimado("Diego: Guilherme... eu senti cada passo seu em meio às trevas.\n", 40000);
+    textoAnimado("Mesmo aprisionado, sua lógica me alcançava como um sussurro distante.\n", 40000);
+    textoAnimado("Você trouxe de volta aquilo que nem eu acreditava restar: a última luz.\n\n", 40000);
+
+    textoAnimado("Guilherme: Diego... eu nunca deixei de acreditar em você. Sua chama sempre me guiou.\n\n", 40000);
+
+    textoAnimado("Diego observa as próprias mãos, redescobrindo sua existência.\n", 40000);
+    textoAnimado("Diego: Eu pensei que minha luz havia sido apagada. Mas você provou que a lógica é vínculo, memória e esperança.\n", 40000);
+    textoAnimado("Sua alma encontrou a minha novamente. Por isso despertei.\n\n", 40000);
+
+    textoAnimado("A chama ao redor de Diego cresce, iluminando o rosto de Guilherme.\n\n", 40000);
+
+    textoAnimado("Guilherme: Eu prometi que te traria de volta. Agora... o mundo pode respirar outra vez.\n\n", 40000);
+
+    textoAnimado("Diego sorri, iluminado.\n", 40000);
+    textoAnimado("Diego: A luz lógica renasce em todos que ousam pensar, questionar e iluminar o que as trevas tentam ocultar.\n", 40000);
+    textoAnimado("Você devolveu o sentido à equivalência. E por isso, eu agradeço.\n", 40000);
+    textoAnimado("Com toda a luz que ainda há em mim.\n\n", 40000);
+
+    textoAnimado("Guilherme estende a mão. Diego segura.\n", 40000);
+    textoAnimado("Uma onda de luz varre o cenário, limpando todo o caos.\n", 40000);
+    textoAnimado("A terra se recompõe. O ar volta a aquecer. O mundo respira.\n\n", 40000);
+
+    textoAnimado("Guilherme: Estamos juntos, Diego. Sempre estivemos.\n\n", 40000);
+
+    textoAnimado("Diego aperta sua mão, firme.\n", 40000);
+    textoAnimado("Diego: Então vamos, Guilherme. Há um mundo esperando para ser reacendido. E desta vez... não caminharemos mais nas trevas.\n\n", 40000);
+
+    textoAnimado("THE LAST EQUIVALENCE: LUMEN OF DIEGO\n\n", 40000);
+}
