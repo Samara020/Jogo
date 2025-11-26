1f190b5d201d1cbccf4a91a7ef54db6786a7f585
Carlos Henrique <carlos.henriquegds79@gmail.com>
2025-11-25 23:34:54 -0300

    movimento

---
+++ include/movimento.h
@@ -0,0 +1,15 @@
+#ifndef MOVIMENTO_H
+#define MOVIMENTO_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include "keyboard.h"
+
+#define SCREEN_RIGHT_LIMIT 140 
+
+void drawKnight(int x, int y);
+void drawRoad();
+void drawArrow();
+void primeiracena();
+
+#endif
